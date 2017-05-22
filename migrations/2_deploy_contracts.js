de21507dd582c283973b4cf864da06ff78109533
Nick Jorens <nickjorens@gmail.com>
2017-05-22 20:34:18 +0200

    Added Truffle

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,17 @@
+var Token = artifacts.require("./token/Token.sol");
+var TokenSale = artifacts.require("./token/TokenSale.sol");
+
+module.exports = function(deployer) {
+
+  var _name = "The Fund";
+  var _symbol = "FND";
+  var _supply = 650000;
+  var _hardcap = 100000
+  var _softcap = 5000;
+  var _price = 5;
+  var _purchaseLimit = 5000;
+
+  deployer.deploy(Token, _name, _symbol, _supply).then(function(){
+    deployer.deploy(TokenSale, _hardcap, _softcap, Token.address, _price, _purchaseLimit );
+  });
+};
