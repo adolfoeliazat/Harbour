2a2bb0774bd053b7a6fe6715f26279f3536c9e38
decanus <dean.eigenmann@icloud.com>
2017-05-23 20:33:18 +0200

    TokenWallet

--- contracts/wallets/TokenWallet.sol
+++ contracts/wallets/TokenWallet.sol
@@ -1,10 +1,17 @@
 pragma solidity ^0.4.11;
 
 import "../ownership/ownable.sol";
-
+import "../tokens/ERC20.sol";
 
 contract TokenWallet is ownable {
 
     function () public payable {}
 
+    function balanceOf(address _token) constant returns (uint) {
+        return ERC20(_token).balanceOf(this);
+    }
+
+    function transfer(address _token, address _to, uint _amount) return (bool) {
+        return ERC20(_token).transfer(_to, _amount);
+    }
 }
