9255f64e1cd77a7ec144563f7c5e9fb165c7e13c
decanus <dean.eigenmann@icloud.com>
2017-05-23 22:04:28 +0200

    TimeLockedWallet added

---
+++ contracts/wallets/TimeLockedWallet.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.11;
+
+import "../ownership/ownable.sol";
+
+contract TimeLockedWallet is ownable {
+
+    uint public lockedUntil;
+
+    function TimeLockedWallet(uint lockForDays) {
+        lockedUntil = now + lockForDays * 1 days;
+    }
+
+    function withdraw() onlyOwner {
+        if (now < lockedUntil) throw;
+        if (!msg.sender.send(this.balance)) throw;
+    }
+}
