1bfd6fe01e8bf8cf69e1ae8a0b132b4a4c0a902b
Dean Eigenmann <dean.eigenmann@icloud.com>
2017-06-26 07:33:11 +0200

    enhancement/remove-tokensale (#18)
    * Removed crowdsale relevant stuff
    
    * remove from truffle

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,21 +1,6 @@
 var SafeMath = artifacts.require('SafeMath.sol');
-var Token = artifacts.require("./tokens/Token.sol");
-var TokenSale = artifacts.require("./tokens/TokenSale.sol");
 var Configuration = artifacts.require("./dao/Configuration.sol");
 
 module.exports = async (deployer) => {
-
-  var _name = "Harbour";
-  var _symbol = "HRB";
-  var _hardcap = 50000;
-  var _softcap = 5000;
-  var _price = 10;
-  var _purchaseLimit = 5000;
-
-  deployer.deploy(SafeMath);
-  await deployer.deploy(Token, _name, _symbol);
-  await deployer.deploy(TokenSale, _hardcap, _softcap, Token.address, _price, _purchaseLimit);
-
-  deployer.link(SafeMath, [Token, TokenSale]);
   deployer.deploy(Configuration);
 };
