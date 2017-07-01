7854609671bf68da69dfd8b1803b38fa2e16a15f
decanus <dean.eigenmann@icloud.com>
2017-07-02 00:10:13 +0200

    Created DAO change executor

---
+++ contracts/congress/executors/DaoChangeExecutor.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.11;
+
+import "./Executor.sol";
+import "../../Configuration.sol";
+import "../proposals/Proposal.sol";
+import "../proposals/DaoChangeProposal.sol";
+import "../../ownership/ownable.sol";
+
+contract DaoChangeExecutor is ownable {
+
+    Configuration public configuration;
+
+    function DaoChangeExecutor(Configuration _configuration) {
+        configuration = _configuration;
+    }
+
+    function proposalType() returns (Proposal.ProposalType) {
+        return Proposal.ProposalType.DAO_CHANGE;
+    }
+
+    function execute(Proposal _proposal) onlyOwner returns (bool) {
+        proposal = DaoChangeProposal(_proposal);
+        (name, value) = proposal.getChange();
+        configuration.set(name, value);
+    }
+
+    function destroy() onlyOwner {
+        selfdestruct(owner);
+    }
+}
